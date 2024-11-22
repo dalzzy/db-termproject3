44ad82b515898747456a546ad6d23ab925470f16
gusrh <gusrh@DESKTOP-1THUMMK>
2024-11-22 19:42:13 +0900

    DTO 파일들 (getter와 setter)

--- src/backend/comment/CommentDTO.java
+++ src/backend/comment/CommentDTO.java
@@ -1,4 +1,59 @@
 package backend.comment;
 
 public class CommentDTO {
+	private int commentId;
+    private int userId;
+    private int postId;
+    private String content;
+    private String createdAt;
+    private String updatedAt;
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+	
 }
